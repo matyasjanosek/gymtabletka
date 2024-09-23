3b15fb4e98853a7a9e4849006b908b5cbcd3d8b0
Jakub <jakrep01@easv365.dk>
2024-09-23 16:12:35 +0200

    footer dynamic

--- footer.php
+++ footer.php
@@ -28,30 +28,34 @@ Template Name: Footer
         </div>
         <div class="flex flex-col justify-start text-white">
             <p class="text-lg font-bold text-white">
-                Need help buying
+            <?php the_field('help_title'); ?>
             </p>
             <p class="text-[#bdc1c5]">
-                We can be reached every day between 7:00 and 15:00
+            <?php the_field('help_text'); ?>
             </p>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                     <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
                 </svg>
-                <p class="pl-2 font-bold text-[#bdc1c5]">+45 50 34 00 20</p>
+                <p class="pl-2 font-bold text-[#bdc1c5]">
+                <?php the_field('contact_number'); ?>
+                </p>
             </div>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                     <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
                     <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
                 </svg>
-                <p class="pl-2 font-bold text-[#bdc1c5]">info@yourcompany.com</p>
+                <p class="pl-2 font-bold text-[#bdc1c5]">
+                <?php the_field('contact_email'); ?>
+                </p>
             </div>
         </div>
         <div class="text-[#bdc1c5]">
             <div class="pb-4 text-4xl font-bold text-white">LOGO</div>
-            <p>GymTabletka</p>
-            <p>Spangsbjerg Kirkevej 103,</p>
-            <p>6700 Esbjerg, Denmark</p>
+            <p><?php the_field('company_name'); ?></p>
+            <p><?php the_field('company_address'); ?></p>
+            <p><?php the_field('company_city'); ?></p>
             <div class="flex items-center pt-4">
                 <svg id='Facebook_24' width='40' height='40' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
                     <rect width='24' height='24' stroke='none' fill='#000000' opacity='0'/>
@@ -77,7 +81,7 @@ Template Name: Footer
     <div class="container flex justify-between max-w-6xl py-8 mx-auto">
         <div class="flex items-center">
             <img src="<?php echo get_template_directory_uri(); ?>/images/googleLogo.png" alt="GoogleLogo" class="w-16 h-auto">
-            <p class="text-[#bdc1c5] pl-1 font-bold">GymTabletka</p>
+            <p class="text-[#bdc1c5] pl-1 font-bold"><?php the_field('company_name'); ?></p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" fill="#F9BF3B" class="size-5">
                 <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
             </svg>
@@ -90,7 +94,7 @@ Template Name: Footer
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" fill="#F9BF3B" class="size-5">
                 <path fill-rule="evenodd" d="M8 1.75a.75.75 0 0 1 .692.462l1.41 3.393 3.664.293a.75.75 0 0 1 .428 1.317l-2.791 2.39.853 3.575a.75.75 0 0 1-1.12.814L7.998 12.08l-3.135 1.915a.75.75 0 0 1-1.12-.814l.852-3.574-2.79-2.39a.75.75 0 0 1 .427-1.318l3.663-.293 1.41-3.393A.75.75 0 0 1 8 1.75Z" clip-rule="evenodd" />
             </svg>
-            <p class="text-[#bdc1c5] pl-1">3,352 Google reviews</p>
+            <p class="text-[#bdc1c5] pl-1"><?php the_field('reviews'); ?></p>
         </div>
         <div class="flex space-x-1">
             <img src="<?php echo get_template_directory_uri(); ?>/images/visa.jpg" alt="VISA" class="h-auto rounded-md w-14">
